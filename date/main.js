348c33a0cf219767b592a0e50bacdd09fd851ab4
AlionaTytarenko <47102384+AlionaTytarenko@users.noreply.github.com>
2019-12-09 15:04:42 +0200

    Update main.js

--- date/main.js
+++ date/main.js
@@ -25,8 +25,76 @@ function monthLength(month) {
 
 // date: string, format dd.MM.YYYY
 function dayOfWeek(date) {
-    //TODO
-    // for inspiration
-    // https://artofmemory.com/blog/how-to-calculate-the-day-of-the-week-4203.html
-    // https://wpcalc.com/kak-uznat-den-nedeli-lyuboj-daty/
+    let arr = "date".split(".");
+    let day = arr[0];
+    let month = arr[1];
+    let year = arr[2];
+
+    century = Math.round(year/100);
+
+    if ([3, 7, 11, 15, 19].includes(century)) {
+        centuryShift = 0
+        }
+    if ([4, 8, 12, 16, 20].includes(century)) {
+        centuryShift = 6
+        }
+    if ([1, 5, 9, 13, 17, 21].includes(century)) {
+        centuryShift = 4
+        }
+    if ([2, 6, 10, 14, 18].includes(century)) {
+        centuryShift = 2
+        }
+
+    decimalYear = (year - (Math.round(year/100));
+    
+    yearShift = ((decimalYear + (decimalYear/4)) % 7);
+
+    if ([01, 10].includes(month)) {
+        monthShift = 0
+        }
+    if ([02, 03, 11].includes(month)) {
+        monthShift = 3
+        }
+    if ([04, 07].includes(month)) {
+        monthShift = 6
+        }
+    if ([05].includes(month)) {
+        monthShift = 1
+        }
+    if ([06].includes(month)) {
+        monthShift = 4
+        }
+    if ([08].includes(month)) {
+        monthShift = 2
+        }
+    if ([09, 12].includes(month)) {
+        monthShift = 5
+        }
+
+    dayShift = (day % 7);
+    
+    sumShift = (centuryShift + yearShift + monthShift + dayShift);
+    dayOfWeek = Math.round(sumShift/7);
+
+    if (dayOfWeek == 1) {
+        return 'Monday'
+        }
+    if (dayOfWeek == 2) {
+        return 'Tuesday'
+        }
+    if (dayOfWeek == 3) {
+        return 'Wednesday'
+        }
+    if (dayOfWeek == 4) {
+        return 'Thursday'
+        }
+    if (dayOfWeek == 5) {
+        return 'Friday'
+        }
+    if (dayOfWeek == 6) {
+        return 'Saturday'
+        }
+    if (dayOfWeek == 7) {
+        return 'Sunday'
+        }
 }
